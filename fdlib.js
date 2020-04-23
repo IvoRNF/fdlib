62d58d358329de798a8e4536c5c2ab45a4382213
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-04-23 17:05:02 -0300

    adicionado release

--- fdlib.js
+++ fdlib.js
@@ -3,12 +3,14 @@ var ffi = require('ffi-napi')
  
 var lib = ffi.Library('./node_modules/fdlib-win-x64/FDLib.dll', {
   'query': [ 'string',['string','string'] ],
-  'update' : [ 'string',['string','string'] ]  
+  'update' : [ 'string',['string','string'] ] ,
+  'release' : [ 'string',['string'] ]  
 })
 
 
 exports.query = lib.query
 exports.update = lib.update
+exports.release = lib.release
 
 
 
